e9ac9ad98198899a330c15eb50aa3d2b4ed77310
Aswin-S-T <aswinswebart15@gmail.com>
2024-12-25 11:40:18 +0530

    project starred

--- App.js
+++ App.js
@@ -1,20 +1,82 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { Ionicons } from "@expo/vector-icons";
+import { createAppContainer } from "react-navigation";
+import { createMaterialBottomTabNavigator } from "react-navigation-material-bottom-tabs";
+import SearchScreen from "./src/screens/SearchScreen/SearchScreen";
+import NotificationScreen from "./src/screens/NotificationScreen/NotificationScreen";
+import Cars from "./src/screens/Cars/Cars";
+import HomeScreen from "./src/screens/HomeScreen/HomeScreen";
+import Entypo from "@expo/vector-icons/Entypo";
+import Feather from "@expo/vector-icons/Feather";
+import AntDesign from "@expo/vector-icons/AntDesign";
+
+const TabNavigator = createMaterialBottomTabNavigator(
+  {
+    Home: {
+      screen: HomeScreen,
+      navigationOptions: {
+        tabBarLabel: "Home",
+        tabBarIcon: (tabInfo) => (
+          <Entypo
+            name="home"
+            size={tabInfo.focused ? 26 : 20}
+            color={tabInfo.focused ? "rgb(222, 49, 99)" : "grey"}
+          />
+        ),
+      },
+    },
+    Search: {
+      screen: SearchScreen,
+      navigationOptions: {
+        tabBarLabel: "Search",
+        tabBarIcon: (tabInfo) => (
+          <Feather
+            name="search"
+            size={tabInfo.focused ? 26 : 20}
+            color={tabInfo.focused ? "rgb(222, 49, 99)" : "grey"}
+          />
+        ),
+      },
+    },
+    Cars: {
+      screen: Cars,
+      navigationOptions: {
+        tabBarLabel: "Explore",
+        tabBarIcon: (tabInfo) => (
+          <AntDesign
+            name="car"
+            size={tabInfo.focused ? 26 : 20}
+            color={tabInfo.focused ? "rgb(222, 49, 99)" : "grey"}
+          />
+        ),
+      },
+    },
+    Notification: {
+      screen: NotificationScreen,
+      navigationOptions: {
+        tabBarLabel: "Notification",
+        tabBarIcon: (tabInfo) => (
+          <Ionicons
+            name="notifications"
+            size={tabInfo.focused ? 26 : 20}
+            color={tabInfo.focused ? "rgb(222, 49, 99)" : "grey"}
+          />
+        ),
+      },
+    },
+  },
+  {
+    initialRouteName: "Home",
+    barStyle: { backgroundColor: "#fff" },
+  }
+);
+
+const Navigator = createAppContainer(TabNavigator);
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Navigator>
+      <HomeScreen />
+    </Navigator>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ src/screens/Cars/Cars.jsx
@@ -0,0 +1,14 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import { Ionicons } from "@expo/vector-icons";
+
+const Cars = () => {
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+    <Text style={{ color: "#006600", fontSize: 40 }}>Home Screen!</Text>
+    <Ionicons name="md-home" size={80} color="#006600" />
+  </View>
+  )
+}
+
+export default Cars
